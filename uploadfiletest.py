79da09069707c4f2f2b1683b36d3243fd67b3a8b
alexgonca <alexandre.reportagem@gmail.com>
2017-08-10 12:21:30 -0400

    initial commit

---
+++ uploadfiletest.py
@@ -0,0 +1,12 @@
+class Importable:
+    @staticmethod
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def event():
+        pass
+
+    @staticmethod
+    def render(wf_module, table):
+        return None
